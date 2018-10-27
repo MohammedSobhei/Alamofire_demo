f16b2ab0b6bcf50ce815f941a960cf879dbb465e
Mohammed S. Abdul All <m.sf2009@hotmail.com>
2018-10-27 10:30:16 +0200

    Initial Commit

---
+++ WebServiceDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebServiceDemo
+//
+//  Created by Mohammed Sobhei Abdualal on 10/27/18.
+//  Copyright © 2018 Mohammed Sobhei Abdualal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
